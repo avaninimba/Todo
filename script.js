91fdb900617baff9986a20bf9195797a9e5bf88d
avaninimba <32764673+avaninimba@users.noreply.github.com>
2017-11-19 23:47:50 +0100

    added log in and log out

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
-    $("#loginbutton").click(function(){
-      sessionStorage.mittnamn = "Avani Nimba";
-      document.getElementById("#p1").innerHTML= sessionStorage.mittnamn;
-      });
-  
+        // on click Sign In Button checks that username =='admin' and password == 'password'
+
+        //sessionStorage.mittnamn = "admin";
+        //sessionStorage.mittpass = "password";
+        $("#login").click(function(){
+        if( $("#User").val()=='admin' && $("#Pass").val()=='password') {
+                $("#welcome").hide();
+                $("#loggedin").append("<p>Hello, admin</p> <br/><input type='button' id='logout' value='Log Out' />");
+            }
+        else {
+            $("#welcome").append("<p>Hello, please try again</p>");
+        }
+
+        $("#loggedout").click(function() {
+        $("#form")[0].reset();
+        $("#welcome").show();
+        $("#loggedin").hide();
+        
     });
+});
+
+});
