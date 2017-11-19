431b3b9c3c006c9bf516e4c99b315c6f49a961e7
avaninimba <32764673+avaninimba@users.noreply.github.com>
2017-11-19 22:18:50 +0100

    added text boxes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $("button").click(function(){
+    $("loginbutton").click(function(){
       sessionStorage.mittnamn = "Avani Nimba";
-      document.getElementById("showname").innerHTML= sessionStorage.mittnamn;
+      document.getElementById("main").innerHTML= sessionStorage.mittnamn;
       });
   
     });
