d14862b9f3f89a5cca27b0c4f51546cd96cb1e19
avaninimba <32764673+avaninimba@users.noreply.github.com>
2017-11-20 00:43:42 +0100

    Added fotter and log out messgae

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
         // on click Sign In Button checks that username =='admin' and password == 'password'
 
-        sessionStorage.mittnamn = "admin";
+        sessionStorage.mittnamn = "test";
         sessionStorage.mittpass = "password";
 
         var stuffToDo = [
@@ -17,7 +17,7 @@ $(document).ready(function(){
         if( $("#User").val()==sessionStorage.mittnamn && $("#Pass").val()==sessionStorage.mittpass) {
                 //$("#welcome").hide();
                 $("#loggedin").append("<input type='button' id='logout' value='Log Out' />");
-                $("#p1").text("Hello, admin! <br>Thank you for logging in");
+                $("#p1").text("Hello, test! <br>Thank you for logging in");
                 $("#p2").text(str);
             }
         else {
@@ -25,10 +25,11 @@ $(document).ready(function(){
             $("#p2").text("Har du glömt ditt lösenord");
         }
 
-        $("#loggedout").click(function() {
-        $("#form")[0].reset();
-        $("#welcome").show();
-        $("#loggedin").hide();
+        $("#logout").click(function() {
+            $("#p1").text("Welcome to my website");
+            $("#p2").text("Please log in");
+            $("#welcome").show();
+            $("#loggedin").hide();
         
     });
 });
